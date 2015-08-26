6b0a3ff102307e9f72b864aead286a784ae1fd68
yuki <yukisov@gmail.com>
2015-08-27 00:45:40 +0900

    Add output.publicPath in webpack.config.js for using webpack-dev-server as Dev-Web-Server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   },
   output: {
     path: './public/js',
+    // publicPath は webpack-dev-server で自動コンパイルするために必要（URLにおけるJSファイルへのパスを書く）
+    publicPath: '/js/',
     filename: "[name].js"
   },
   module: {
