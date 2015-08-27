f64656a720440c445cdd75cf470d91c915339f80
yuki <yukisov@gmail.com>
2015-08-27 12:04:18 +0900

    Add some codes

--- src/js/entry.js
+++ src/js/entry.js
@@ -5,3 +5,9 @@ require('../css/entry');
 var foo = require('./foo');
 console.log(foo.hello());
 
+// Use file-loader
+var url = require("file!../img/ESLint_logo.png"),
+    img = new Image();
+img.src = url;
+document.body.appendChild(img);
+console.log(url);
